e07b0fa4044005cdb48ee8319e611a24ad58cdc7
Jose Fabio Avila Estrada <fabioavila-16@hotmail.com>
2017-10-29 16:37:36 -0600

    Working on Heroes detail view

--- 2. SPA/src/app/app.module.ts
+++ 2. SPA/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HttpModule } from '@angular/http';
 import { APP_ROUTING } from './app.routes';
 
 // Servicios
-
+import { HeroesService } from './services/heroes.service';
 
 // componentes
 import { AppComponent } from './app.component';
@@ -15,6 +15,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +23,16 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
